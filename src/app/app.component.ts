30236376febf74c779da50d6ebd5ac8d954c46c3
hnah <hnahoulder@gmail.com>
2018-07-16 23:58:37 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+
+import {single} from './data';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    view: any[] = [700, 400];
+    data: any[];
+
+    constructor() {
+        this.data = single;
+    }
+
+    colorScheme = {
+        domain: ['#5AA454', '#A10A28', '#C7B42C']
+    };
+
+    onSelect(event) {
+        console.log(event);
+    }
 }
---
+++ src/app/data.ts
@@ -0,0 +1,14 @@
+export var single = [
+    {
+        'name': 'Réception',
+        'value': 98
+    },
+    {
+        'name': 'Distribution',
+        'value': 54
+    },
+    {
+        'name': 'Vigilance',
+        'value': 5
+    }
+];
